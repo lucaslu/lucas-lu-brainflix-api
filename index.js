b0989afaeb07df8b8729d91b1c3de4ddeb31fc55
Lucas Lu <lucaslu@hotmail.com>
2022-11-14 20:20:15 -0800

    add endpoints for /videos and /videos/:videoId

--- index.js
+++ index.js
@@ -3,9 +3,32 @@ const path = require("node:path");
 
 const app = express();
 
-app.get("/", (req, res) =>
+const videosJSONFile = path.join(__dirname, "./data/videos.json");
+const videos = require(videosJSONFile);
+
+app.get("/", (_req, res) =>
   res.sendFile(path.join(__dirname, "public", "index.html"))
 );
+
+app.get("/videos", (_req, res) => {
+  const newVideos = videos.map(({ id, title, channel, image }) => ({
+    id,
+    title,
+    channel,
+    image,
+  }));
+
+  res.status(200).json(newVideos);
+});
+
+app.get("/videos/:videoId", (req, res) => {
+  const found = videos.find((video) => video.id === req.params.videoId);
+  if (found) {
+    res.status(200).json(found);
+  } else {
+    res.status(404).json({ message: "No video with that id exists" });
+  }
+});
 
 app.listen(8080, () => {
   console.log(`Server is running on port 8080 🚀`);
