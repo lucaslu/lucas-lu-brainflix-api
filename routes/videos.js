4a70c1673e98f9839e2717ed226cfc427340362d
Lucas Lu <lucaslu@hotmail.com>
2022-11-15 20:17:38 -0800

    add Comment creation and deletion

--- routes/videos.js
+++ routes/videos.js
@@ -60,4 +60,57 @@ router.post("/", (req, res) => {
   res.status(201).json(newVideo);
 });
 
+router.post("/:videoId/comments", (req, res) => {
+  const { name, comment } = req.body;
+
+  if (!name || !comment) {
+    return res.status(400).json({
+      error: "Please provide name and comment for adding comment",
+    });
+  }
+
+  const newComment = {
+    id: getNewId(),
+    name,
+    comment,
+    likes: 0,
+    timestamp: Date.now(),
+  };
+
+  const found = videos.find((video) => video.id === req.params.videoId);
+  if (found) {
+    found.comments.push(newComment);
+    writeJSONFile(videosJSONFile, videos);
+    const resComment = {
+      name,
+      comment,
+      id: newComment.id,
+      timestamp: newComment.timestamp,
+    };
+    res.status(200).json(resComment);
+  } else {
+    res.status(404).json({ message: "No video with that id exists" });
+  }
+});
+
+router.delete("/:videoId/comments/:commentId", (req, res) => {
+  const found = videos.find((video) => video.id === req.params.videoId);
+  const foundComment = found.comments.find(
+    (comment) => comment.id === req.params.commentId
+  );
+
+  if (foundComment) {
+    const commentsAfterDeletion = found.comments.filter(
+      (comment) => comment.id !== req.params.commentId
+    );
+    found.comments = commentsAfterDeletion;
+
+    writeJSONFile(videosJSONFile, videos);
+    delete foundComment.likes;
+    res.json({ ...foundComment });
+  } else {
+    res.status(404).json({ errorMessage: "Comment not found" });
+  }
+});
+
 module.exports = router;
